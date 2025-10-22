621fbd9ab7d56f28d2d3e6070b388f9f07a353ed
Artur Assis Guerra <arturassisguerra@gmail.com>
2025-10-21 23:39:56 -0300

    mod 3

--- providers.tf
+++ providers.tf
@@ -1,13 +1,37 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.17.0"
     }
+  }
+  backend "s3" {
+    bucket  = "a2g-grupo-state-bucket-arturgrr-iac"
+    region  = "sa-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = "AdministratorAccess-058264405800"
   }
 }
 
 provider "aws" {
   region  = "sa-east-1"
   profile = "AdministratorAccess-058264405800"
+}
+
+resource "aws_s3_bucket" "a2g_state_bucket" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "a2g_state_bucket_versioning" {
+  bucket = aws_s3_bucket.a2g_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.a2g_state_bucket]
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  description = "Nome do bucket de estado"
+  default     = "a2g-grupo-state-bucket-arturgrr-iac"
+}
